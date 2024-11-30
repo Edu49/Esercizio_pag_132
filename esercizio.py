a5cc8c92754e67b90c89e690b2fbc6d6e4b1e703
Edu49 <eduardgabriel.donisan@iismajoranamoncalieri.edu.it>
2024-11-30 11:30:49 +0000

    Modificato file PDF e aggiustato esercizio

--- esercizio.py
+++ esercizio.py
@@ -4,34 +4,34 @@ while reddito_imp < 0:
     reddito_imp = float(input("Inserisci il tuo reddito: "))
 
 imposta = 0.0
-aliquota12 = 0.12
-aliquota18 = 0.18
-aliquota27 = 0.27
-aliquota48 = 0.48
-aliquota60 = 0.60
+Aliquota12 = 0.12
+Aliquota18 = 0.18
+Aliquota27 = 0.27
+Aliquota48 = 0.48
+Aliquota60 = 0.60
 
 if reddito_imp > 100000:
-    imposta += (reddito_imp - 100000) * aliquota60
-    print(f"Imposta: {(reddito_imp - 100000) * aliquota60} euro.")
+    imposta += (reddito_imp - 100000) * Aliquota60
+    print(f"Imposta: {(reddito_imp - 100000) * Aliquota60} euro.")
     reddito_imp = 100000
     
 if 60000 < reddito_imp <= 100000:
-    imposta += (reddito_imp - 60000) * aliquota48
-    print(f"Imposta: {(reddito_imp - 60000) * aliquota48} euro.")
+    imposta += (reddito_imp - 60000) * Aliquota48
+    print(f"Imposta: {(reddito_imp - 60000) * Aliquota48} euro.")
     reddito_imp = 60000 
     
 if 35000 < reddito_imp <= 60000:
-    imposta += (reddito_imp - 35000) * aliquota27
-    print(f"Imposta: {(reddito_imp - 35000) * aliquota27} euro.")
+    imposta += (reddito_imp - 35000) * Aliquota27
+    print(f"Imposta: {(reddito_imp - 35000) * Aliquota27} euro.")
     reddito_imp = 35000
     
 if 20000 < reddito_imp <= 35000:
-    imposta += (reddito_imp - 20000) * aliquota18
-    print(f"Imposta: {(reddito_imp - 20000) * aliquota18} euro.")
+    imposta += (reddito_imp - 20000) * Aliquota18
+    print(f"Imposta: {(reddito_imp - 20000) * Aliquota18} euro.")
     reddito_imp = 20000
     
 if 10000 < reddito_imp <= 20000:
-    imposta += (reddito_imp - 10000) * aliquota12
-    print(f"Imposta: {(reddito_imp - 10000) * aliquota12} euro.")
+    imposta += (reddito_imp - 10000) * Aliquota12
+    print(f"Imposta: {(reddito_imp - 10000) * Aliquota12} euro.")
     
 print(f"Sul tuo reddito verranno detratte {imposta} euro di imposta.")
